3fa7964c1b469d5881c09cbfb799a77c1f2ab736
Jiramot Numnam <jiramot@gmail.com>
2015-11-17 12:45:30 +0700

    Add repo to seed-job

--- init.groovy.d/create-seed-job.groovy
+++ init.groovy.d/create-seed-job.groovy
@@ -1,6 +1,6 @@
 import hudson.model.FreeStyleProject;
-//import hudson.plugins.git.GitSCM;
-//import hudson.plugins.git.BranchSpec;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.BranchSpec;
 import hudson.triggers.SCMTrigger;
 import jenkins.model.*
 import hudson.plugins.git.*;
@@ -19,12 +19,12 @@ configuration.save();
 jobName = "seed-job";
 gitTrigger = new SCMTrigger("* * * * *");
 dslBuilder = new ExecuteDslScripts(scriptLocation=new ExecuteDslScripts.ScriptLocation(value = "false", 
-  targets="build-pr-from-github,build-github-branch", scriptText=""), 
+  targets="job-dsl/*.groovy", scriptText=""), 
   ignoreExisting=false, 
   removedJobAction=RemovedJobAction.DISABLE);
 seedJobProject = new hudson.model.FreeStyleProject(jenkins, jobName);
-//dslProject.scm = new GitSCM("git@github.com:jiramot/jenkins-docker.git");
-//dslProject.scm.branches = [new BranchSpec("*/master")];
+seedJobProject.scm = new GitSCM("git@github.com:jiramot/jenkins-docker.git");
+seedJobProject.scm.branches = [new BranchSpec("*/master")];
 seedJobProject.getPublishersList().add(dslBuilder);
 jenkins.add(seedJobProject, jobName);
 gitTrigger.start(seedJobProject, true);
