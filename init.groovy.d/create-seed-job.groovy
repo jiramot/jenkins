b3d5b8c1410945835da47067fe037bcc1706d496
Jiramot Numnam <jiramot@gmail.com>
2015-11-17 16:09:57 +0700

    Add volume job-dsl

--- init.groovy.d/create-seed-job.groovy
+++ init.groovy.d/create-seed-job.groovy
@@ -1,10 +1,9 @@
 import hudson.model.FreeStyleProject;
-import hudson.plugins.git.GitSCM;
-import hudson.plugins.git.BranchSpec;
 import hudson.triggers.SCMTrigger;
 import jenkins.model.*
 import hudson.plugins.git.*;
 import javaposse.jobdsl.plugin.*;
+import hudson.plugins.filesystem_scm.*;
 
 
 jenkins = Jenkins.instance;
@@ -17,12 +16,11 @@ configuration.save();
 jobName = "seed-job";
 gitTrigger = new SCMTrigger("* * * * *");
 dslBuilder = new ExecuteDslScripts(scriptLocation=new ExecuteDslScripts.ScriptLocation(value = "false", 
-  targets="job-dsl/*.groovy", scriptText=""), 
+  targets="*.groovy", scriptText=""), 
   ignoreExisting=false, 
   removedJobAction=RemovedJobAction.DISABLE);
 seedJobProject = new hudson.model.FreeStyleProject(jenkins, jobName);
-seedJobProject.scm = new GitSCM("https://github.com/jiramot/jenkins-docker.git");
-seedJobProject.scm.branches = [new BranchSpec("*/master")];
+seedJobProject.scm = new FSSCM("/var/job-dsl", false, false, false, false, null);
 seedJobProject.getPublishersList().add(dslBuilder);
 jenkins.add(seedJobProject, jobName);
 gitTrigger.start(seedJobProject, true);
