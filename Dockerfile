bc5c1453e801ef1d584fe7cd5686defc11d72788
Jiramot Numnam <jiramot@gmail.com>
2016-06-09 00:21:03 +0700

    Update compose version

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,3 @@ USER jenkins
 
 COPY plugins.txt /plugins.txt
 RUN /usr/local/bin/plugins.sh /plugins.txt
-
-COPY init.groovy.d/create-seed-job.groovy /usr/share/jenkins/ref/init.groovy.d/00-create-seed-job.groovy
--- init.groovy.d/create-seed-job.groovy
+++ init.groovy.d/create-seed-job.groovy
@@ -7,7 +7,7 @@ import hudson.plugins.filesystem_scm.*;
 
 
 jenkins = Jenkins.instance;
-jenkins.setNumExecutors(4);
+jenkins.setNumExecutors(1);
 
 configuration = JenkinsLocationConfiguration.get();
 configuration.setUrl(System.getenv("JENKINS_URL"));
@@ -15,12 +15,24 @@ configuration.save();
 
 jobName = "seed-job";
 gitTrigger = new SCMTrigger("* * * * *");
-dslBuilder = new ExecuteDslScripts(scriptLocation=new ExecuteDslScripts.ScriptLocation(value = "false", 
-  targets="*.groovy", scriptText=""), 
-  ignoreExisting=false, 
+
+// Get jobs from mount /var/job-dsl folder (this docker-compose mount ./job-dsl to /var/job-dsl)
+dslBuilder = new ExecuteDslScripts(scriptLocation=new ExecuteDslScripts.ScriptLocation(value = "false",
+  targets="*.groovy", scriptText=""),
+  ignoreExisting=false,
   removedJobAction=RemovedJobAction.DISABLE);
 seedJobProject = new hudson.model.FreeStyleProject(jenkins, jobName);
 seedJobProject.scm = new FSSCM("/var/job-dsl", false, false, false, false, null);
+
+// If you want to pull
+//dslBuilder = new ExecuteDslScripts(scriptLocation=new ExecuteDslScripts.ScriptLocation(value = "false",
+//  targets="job-dsl/*.groovy", scriptText=""),
+//  ignoreExisting=false,
+//  removedJobAction=RemovedJobAction.DISABLE);
+//seedJobProject.scm = new GitSCM("git@github.com:jiramot/jenkins-docker.git");
+//seedJobProject.scm.branches = [new BranchSpec("*/master")];
+
+
 seedJobProject.getPublishersList().add(dslBuilder);
 jenkins.add(seedJobProject, jobName);
 gitTrigger.start(seedJobProject, true);
